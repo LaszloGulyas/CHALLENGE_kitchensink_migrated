fe86e0f8f13f08238c900a3a4b0e661a9e1acc7f
LaszloGulyas <laszlogulyas.jr@gmail.com>
2024-05-02 15:06:26 +1000

    add acceptance tests

---
+++ src/test/java/acceptance/JbossAcceptanceTest.java
@@ -0,0 +1,70 @@
+package acceptance;
+
+import com.laszlogulyas.kitchensink_migrated.KitchensinkMigratedApplication;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = KitchensinkMigratedApplication.class)
+@Disabled
+public class JbossAcceptanceTest {
+
+    private static final String BASE_URL_MEMBERS = "http://localhost:8080/kitchensink/rest/members";
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Test
+    public void testGetSampleUser() {
+        String url = BASE_URL_MEMBERS + "/0";
+        ParameterizedTypeReference<Map<String, Object>> responseType = new ParameterizedTypeReference<>() {
+        };
+
+        ResponseEntity<Map<String, Object>> actualResponseEntity = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+
+        Map<String, Object> actualResponse = actualResponseEntity.getBody();
+        assertAll(
+                () -> assertEquals(HttpStatus.OK, actualResponseEntity.getStatusCode()),
+                () -> assertEquals(0, actualResponse.get("id")),
+                () -> assertEquals("John Smith", actualResponse.get("name")),
+                () -> assertEquals("john.smith@mailinator.com", actualResponse.get("email")),
+                () -> assertEquals("2125551212", actualResponse.get("phoneNumber"))
+        );
+    }
+
+    @Test
+    public void testGetAllUsers() {
+        ParameterizedTypeReference<List<Map<String, Object>>> responseType = new ParameterizedTypeReference<>() {
+        };
+
+        ResponseEntity<List<Map<String, Object>>> actualResponse = restTemplate.exchange(BASE_URL_MEMBERS, HttpMethod.GET, null, responseType);
+
+        Map<String, Object> firstRecord = Objects.requireNonNull(actualResponse.getBody()).getFirst();
+        assertAll(
+                () -> assertEquals(HttpStatus.OK, actualResponse.getStatusCode()),
+                () -> assertNotNull(firstRecord.get("id")),
+                () -> assertNotNull(firstRecord.get("name")),
+                () -> assertNotNull(firstRecord.get("email")),
+                () -> assertNotNull(firstRecord.get("phoneNumber"))
+        );
+    }
+
+    @Test
+    public void testCreateNewUserOK() {
+        Map<String, Object> request = Map.of("name", "Test Test", "email", "test123@test.test", "phoneNumber", "1231231230");
+
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(BASE_URL_MEMBERS, request, String.class);
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
+}
