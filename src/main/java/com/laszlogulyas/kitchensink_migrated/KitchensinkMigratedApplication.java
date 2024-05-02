7c10a9ef33b925e0d2d4d19a0375b2202195bc7f
LaszloGulyas <laszlogulyas.jr@gmail.com>
2024-05-02 14:24:41 +1000

    init repo

---
+++ src/main/java/com/laszlogulyas/kitchensink_migrated/KitchensinkMigratedApplication.java
@@ -0,0 +1,13 @@
+package com.laszlogulyas.kitchensink_migrated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchensinkMigratedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitchensinkMigratedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/laszlogulyas/kitchensink_migrated/KitchensinkMigratedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laszlogulyas.kitchensink_migrated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchensinkMigratedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
